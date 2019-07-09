2b8baa6d62c13a3701aad2f7dc7dbdee4fb950ef
ezen-test <ezen-test@ezen-test-PC>
2019-07-09 18:40:16 +0900

    로그인 처리 by 김민승

--- cwb/src/main/java/com/cwb/finalproject/member/model/MemberService.java
+++ cwb/src/main/java/com/cwb/finalproject/member/model/MemberService.java
@@ -4,6 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 public interface MemberService {
+	//로그인처리에서 사용하는 final변수
+	int LOGIN_OK=1;			//로그인 성공
+	int PWD_DISAGREE=2;		//비밀번호 불일치
+	int ID_NONE=3;			//아이디 존재하지 않음
+		
 	public List<Map<String, Object>> selectByDeptNo(int deptNo);
+	
 	public Map<String, Object> selectByNo(int memNo);
+	
+	public int loginCheck(String memId, String memPwd);
+	
+	public MemberVO selectByUserid(String memId);
 }
--- cwb/src/main/java/com/cwb/finalproject/member/model/MemberServiceImpl.java
+++ cwb/src/main/java/com/cwb/finalproject/member/model/MemberServiceImpl.java
@@ -1,20 +1,77 @@
 package com.cwb.finalproject.member.model;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cwb.finalproject.common.AES256Util;
 
 @Service
 public class MemberServiceImpl implements MemberService{
 	@Autowired
 	private MemberDAO memberDAO;
+	AES256Util aes256Util;
 	
 	public List<Map<String, Object>> selectByDeptNo(int deptNo){
 		return memberDAO.selectByDeptNo(deptNo);
 	}
 	public Map<String, Object> selectByNo(int memNo){
 		return memberDAO.selectByNo(memNo);
+	}
+	@Override
+	public int loginCheck(String memId, String memPwd) {
+		String dbPwd = memberDAO.selectForLogin(memId);
+		try {
+			aes256Util = new AES256Util(AES256Util.KEY);
+			//dbPwd = aes256Util.aesEncode(dbPwd);
+			dbPwd = aes256Util.aesDecode(dbPwd);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int result = 0;
+		if(dbPwd==null || dbPwd.isEmpty()) {
+			result=ID_NONE;
+		}else {
+			if(dbPwd.equals(memPwd)) {
+				result=LOGIN_OK;
+			}else {
+				result=PWD_DISAGREE;
+			}
+		}
+		return result;
+	}
+	@Override
+	public MemberVO selectByUserid(String memId) {
+		return memberDAO.selectByUserid(memId);
 	}	
 }
