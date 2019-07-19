b559dbc6f4aa314ecad7a9ff5719feb40b245d9c
ezen-test <ezen-test@ezen-test-PC>
2019-07-19 18:22:32 +0900

    임의의 랜덤 문자열 메서드 완료, 메일 보내기 완료, 메일함 조회, 페이지처리 완료, 메일 삭제 처리 미완 by 오승주

--- cwb/src/main/java/com/cwb/finalproject/address/model/EmailService.java
+++ cwb/src/main/java/com/cwb/finalproject/address/model/EmailService.java
@@ -1,5 +1,15 @@
 package com.cwb.finalproject.address.model;
 
+import java.util.List;
+
 public interface EmailService {
 	public int insertEmail(EmailVO vo);
+	public List<EmailVO> sendMailList(EmailVO vo);
+	public int totalCount(int memNo);
+	public EmailVO selectDetail(int mailNo);
+	public int selectNext(int mailNo);
+	public int selectPre(int mailNo);
+	public int countNext(int mailNo);
+	public int countPre(int mailNo);
+	public int deleteMail(int mailNo);
 }
